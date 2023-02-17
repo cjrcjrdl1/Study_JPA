311b4661e06dbcf0779154925b551df173d69848
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-02-17 20:52:39 +0900

    다대다

--- ex1-hello-jpa/src/main/java/hellojpa/Product.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Product.java
@@ -5,27 +5,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Team {
+public class Product {
 
     @Id
     @GeneratedValue
-    @Column(name = "TEAM_ID")
     private Long id;
 
-    @OneToMany(mappedBy = "team")
-    private List<Member> members = new ArrayList<>();
     private String name;
 
-    public Team() {
-    }
-
-    public List<Member> getMembers() {
-        return members;
-    }
-
-    public void setMembers(List<Member> members) {
-        this.members = members;
-    }
+    @OneToMany(mappedBy = "product")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -42,5 +31,4 @@ public class Team {
     public void setName(String name) {
         this.name = name;
     }
-
 }
