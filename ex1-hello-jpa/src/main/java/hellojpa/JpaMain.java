306b60f34450df173d6be525c1795a69680caf74
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-02-17 22:18:29 +0900

    Mapped Superclass -매핑 정보 상속

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 
 public class JpaMain {
@@ -18,15 +17,7 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team);
-
-            Member member = new Member();
-            member.setUsername("member1");
-            em.persist(member);
-
-
+            Movie movie = new Movie();
 
 
             tx.commit();
