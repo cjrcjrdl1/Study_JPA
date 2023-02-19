b24c4d2113063edb30a85346adbe78618bb0706d
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-02-19 22:55:43 +0900

    소개

--- ex1-hello-jpa/src/main/java/jpabook/jpabook/JpaMain.java
+++ ex1-hello-jpa/src/main/java/jpabook/jpabook/JpaMain.java
@@ -1,11 +1,16 @@
 package jpabook.jpabook;
 
 import jpabook.jpabook.jpashop.domain.Book;
+import jpabook.jpabook.jpashop.domain.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -17,10 +22,15 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Book book = new Book();
-            book.setName("JPA");
-            book.setAuthor("A");
-            em.persist(book);
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
+
+            Root<Member> m = query.from(Member.class);
+
+            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("name"), "kim"));
+            List<Member> resultList = em.createQuery(cq)
+                    .getResultList();
+
 
             tx.commit();
         } catch (Exception e) {
