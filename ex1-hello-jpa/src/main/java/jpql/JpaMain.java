02bb79f3a23659a89160c1f5904ec1985efc8c15
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-02-20 11:08:06 +0900

    기본 문법과 쿼리 API

---
+++ ex1-hello-jpa/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,42 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+            List<Member> resultList = query.getResultList();
+            for (Member member1 : resultList) {
+                System.out.println("member1 = " + member1);
+            }
+            Query query1 = em.createQuery("select m.username, m.age from Member m");
+            Object singleResult = query1.getSingleResult();
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally{
+            em.close();
+        }
+
+        emf.close();
+    }
+
+
+}
